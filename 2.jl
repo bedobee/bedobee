c5aa51a9a18b94efa7b6fae5753daa442545de91
bedobee <115418607+bedobee@users.noreply.github.com>
2022-12-25 16:39:54 +0300

    Add files via upload

--- 2.jl
+++ 2.jl
@@ -1,19 +1,21 @@
 using HorizonSideRobots
-function perimeter!(robot)
-    corner!(robot)
-    for side in (Ost, Nord, West, Sud)
-        while !isborder(robot, side)
-            move!(robot, side)
-            putmarker!(robot)
-        end
+function cross(robot)
+    putmarker!(robot)
+    for side in(Nord,Sud,Ost,West)
+        along_and_gohome(robot,side)
     end
 end
 
-function corner!(robot)
-    while !isborder(robot, Sud)
-        move!(robot, Sud)
+function along_and_gohome(robot,side)
+    num_steps = 0
+    while !isborder(robot,side)
+        move!(robot,side)
+        putmarker!(robot)
+        num_steps += 1
     end
-    while !isborder(robot, West)
-        move!(robot, West)
+    for i in 1:num_steps
+        move!(robot,inverse(side))
     end
 end
+
+inverse(side::HorizonSide)::HorizonSide = HorizonSide((Int(side)+2)%4)
