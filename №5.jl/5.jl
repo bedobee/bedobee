c9567d47309137ed0c6731bed7fa617e4a7198b4
bedobee <115418607+bedobee@users.noreply.github.com>
2022-12-26 03:09:43 +0300

    Add files via upload

---
+++ №5.jl/5.jl
@@ -0,0 +1,17 @@
+using HorizonSideRobots
+function around!(robot)
+   corner!(robot)
+end
+
+
+function corner!(robot)
+    while !isborder(robot, Sud)
+        move!(robot, Sud)
+    end
+    while !isborder(robot, West)
+        move!(robot, Ost)
+    end
+end
+
+
+inverse(side::HorizonSide) = HorizonSide((Int(side) +2)% 4)
