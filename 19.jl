83a305c367dc200e7211df988549021459d88d54
bedobee <115418607+bedobee@users.noreply.github.com>
2022-12-26 13:23:24 +0300

    Create 19.jl

---
+++ 19.jl
@@ -0,0 +1,15 @@
+#Задача 19. Написать рекурсивную функцию, перемещающую робота до упора в заданном направлении.
+
+#Ввести в терминал:
+#>julia
+#using HorizonSideRobots
+#include("19.jl")
+#r=Robot("19.sit",animate=true)
+#move_recursion!!(r,Ost)
+
+function move_recursion!(robot,side) #Идет рекурсивно до стены в заданном направлении.
+    if (!isborder(robot,side))
+        move!(robot,side)
+        move_recursion!(robot,side)
+    end
+end
